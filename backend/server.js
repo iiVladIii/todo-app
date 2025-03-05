1fd9f9a417cb0e67e75f0b126cd1d313cfb19622
iiVladIii <vladislav2407@icloud.com>
2025-03-05 15:51:57 +0300

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 require('./utils/env');
 const express = require('express');
+const cors = require('cors'); // Импортируем пакет cors
 const sequelize = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const todoRoutes = require('./routes/todoRoutes');
 const { port } = require('./config/config');
 
 const app = express();
+
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
